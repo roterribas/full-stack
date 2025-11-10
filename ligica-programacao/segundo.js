8a21faef4fad1d955622791a106b574f0f111e58
roterribas <roterribas@uol.com.br>
2025-11-10 15:53:54 -0300

    criando o arquivo segunda.js

--- ligica-programacao/segundo.js
+++ ligica-programacao/segundo.js
@@ -1,6 +1,12 @@
-let numero1 = prompt("DIGITE UM NUMERO");
-let numero2 = prompt("DIGITE OUTRO NUMERO");
+let numero1 = Number(prompt("DIGITE UM NÚMERO"));
+let numero2 = Number(prompt("DIGITE OUTRO NÚMERO"));
 
- let resultado = numero1 + numero2;
+let soma = numero1 + numero2;
+let subtracao = numero1 - numero2;
+let multiplicacao = numero1 * numero2;
+let divisao = numero1 / numero2;
 
-console.log("O RESULTADO DA EQUAÇÃO " + numero1 + "+" + numero2 + "É: " + resultado )
+console.log("O RESULTADO DA SOMA " + numero1 + " + " + numero2 + " É: " + soma);
+console.log("O RESULTADO DA SUBTRAÇÃO " + numero1 + " - " + numero2 + " É: " + subtracao);
+console.log("O RESULTADO DA MULTIPLICAÇÃO " + numero1 + " * " + numero2 + " É: " + multiplicacao);
+console.log("O RESULTADO DA DIVISÃO " + numero1 + " / " + numero2 + " É: " + divisao);
