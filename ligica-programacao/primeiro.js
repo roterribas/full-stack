d275b2e4220779a382c8d5930e57a9eac0efb052
roterribas <roterribas@uol.com.br>
2025-11-10 15:06:36 -0300

    fazendo exercicios de logica de programação

---
+++ ligica-programacao/primeiro.js
@@ -0,0 +1,9 @@
+// PERGUNTANDO O NOME DO USUARIO
+const userName = prompt ("QUAL O SEU NOME?");
+
+// PERGUNTANDO A IDADE DO USUARIO
+const userAge = prompt ("QUAL A SUA IDADE?");
+
+// IMPRIMIMENDO NA TELA O RESULTADO
+document.write("SEU NOME É: " + "<strong>" + userName + "</strong>" + "<br>");
+document.write("SUA IDADE É: " + "<strong>" + userAge + "</strong>" + " ANOS");
---
+++ ligica-programacao/segundo.js
@@ -0,0 +1,6 @@
+let numero1 = prompt("DIGITE UM NUMERO");
+let numero2 = prompt("DIGITE OUTRO NUMERO");
+
+ let resultado = numero1 + numero2;
+
+console.log("O RESULTADO DA EQUAÇÃO " + numero1 + "+" + numero2 + "É: " + resultado )
