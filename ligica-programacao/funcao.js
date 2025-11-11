8f6fc30147cf6d99d2da1c161180ee19a8aea45d
roterribas <roterribas@uol.com.br>
2025-11-11 16:17:07 -0300

    adicionando novos arquivos de logica

---
+++ ligica-programacao/funcao.js
@@ -0,0 +1,12 @@
+function resultAnimals() {
+    console.log("ANIMAIS");
+}
+
+function timeFutebol(time) {
+    document.write(time);
+    document.write("<br>");
+}
+
+resultAnimals();
+timeFutebol("flamengo");
+timeFutebol("paulista");
