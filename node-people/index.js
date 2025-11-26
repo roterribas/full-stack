117ef6ad79be1fca3b02a0743fd1ea271aa5b390
roterribas <roterribas@uol.com.br>
2025-11-26 14:54:49 -0300

    criando novo progeto e duas rotas

---
+++ node-people/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const PORT = 3000; // executar na porta 3000
+
+// mock
+const nomes = [
+  { id: 1, nome: "Fernanda", idade: "18" },
+  { id: 2, nome: "Caio", idade: "23" },
+  { id: 3, nome: "Pedro", idade: "56" },
+  { id: 4, nome: "Samuel", idade: "45" },
+  { id: 5, nome: "Doris", idade: "33" },
+];
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando no endereço http://localhost:${PORT}`);
+});
+
+app.get("/teste", (req, res) => {
+  res.send("API nodePeople está funcionando!");
+});
+
+app.get("/listaNomes", (req, res) => {
+  res.send(nomes);
+});
+
+
+
